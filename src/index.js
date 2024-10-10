1a15a5c3843209c2a10c55dc0d93086391b91174
TrojanVoid <kiroglu.berk@gmail.com>
2024-10-10 21:06:44 +0300

    update index.js amplify import path

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import { Amplify } from 'aws-amplify';
-import amplifyconfig from './src/amplifyconfiguration.json';
-Amplify.configure(amplifyconfig);
-
+import amplifyconfig from './amplifyconfiguration.json';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
@@ -9,6 +7,9 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+
+Amplify.configure(amplifyconfig);
+
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
