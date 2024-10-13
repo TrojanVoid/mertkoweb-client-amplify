efa91e01ea6521913025fa30ffd2f9bb9150f3ab
TrojanVoid <kiroglu.berk@gmail.com>
2024-10-13 16:25:13 +0300

    header changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,14 +4,13 @@ import { HashLink } from 'react-router-hash-link';
 import { Link } from 'react-router-dom';
 import "../style/components/Header.scss";
 
-
-const Header = ({activeTabIndex=0}) => {
-
+const Header = ({ activeTabIndex = 0 }) => {
   const [isMinimized, setIsMinimized] = useState(false);
+  const [isSidePanelOpen, setIsSidePanelOpen] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
-      if (window.scrollY > window.innerHeight * 0.15) { 
+      if (window.scrollY > window.innerHeight * 0.15) {
         setIsMinimized(true);
       } else {
         setIsMinimized(false);
@@ -19,32 +18,55 @@ const Header = ({activeTabIndex=0}) => {
     };
 
     window.addEventListener('scroll', handleScroll);
+
+    const handleClickOutside = (e) => {
+      if (isSidePanelOpen && !e.target.closest('.side-panel') && !e.target.closest('.navbar-toggler')) {
+        setIsSidePanelOpen(false);
+      }
+    };
     
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, []);
+    window.addEventListener('click', handleClickOutside);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener('click', handleClickOutside);
+    };
+  }, [isSidePanelOpen]);
 
   return (
-    <Navbar className={`header ${isMinimized ? 'minimized' : ''} d-flex flex-column justify-content-center align-items-center` } expand="lg">
-      <div className="small-navbar w-100 d-flex flex-row-reverse justify-content-start align-items-center">
-        <a href="tel:+902122230501" >
-          <span className="small-link">Tel: +90 212 223 05 01</span></a>
-        <a href="mailto:mertko@mertko.com" className="footer-email-link">
-          <span className="small-link">E-posta: mertko@mertko.com</span>
-        </a>
+    <Navbar className={`header ${isMinimized ? 'minimized' : ''}`} expand="lg">
+      <div className="small-navbar">
+        <a href="tel:+902122230501">Tel: +90 212 223 05 01</a>
+        <a href="mailto:mertko@mertko.com" className="footer-email-link">E-posta: mertko@mertko.com</a>
       </div>
-      <Container className="large-navbar d-flex justify-content-center align-items-center">
+
+      <Container className="large-navbar">
         <Navbar.Brand className="logo">
-            <HashLink as={Link} className="logo-link" to="/#home">
-              <img src="/resources/images/mertko_logo-300x117.png" alt="Logo" className="logo-image" />
-            </HashLink>
+          <HashLink as={Link} className="logo-link" to="/#home">
+            <img src="/resources/images/mertko_logo-300x117.png" alt="Logo" className="logo-image" />
+          </HashLink>
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" className="d-lg-none" />
-        <Navbar.Collapse className="d-lg-flex flex-row-reverse justify-content-between" id="basic-navbar-nav">
-          <Nav className="navbar-container ml-auto d-lg-flex justify-content-between">
-            <HashLink className={activeTabIndex == 0 ? "active" : null} as={Link} to="/#home">Ana Sayfa</HashLink>
-            <HashLink className={activeTabIndex == 1 ? "active" : null} to="/#about">Hakkımızda</HashLink>            
-            <Nav.Link className={activeTabIndex == 2 ? "active" : null} as={Link} to="/products">Ürünler</Nav.Link>
-            <HashLink className={activeTabIndex == 3 ? "active" : null} as={Link} to="/#contact">İletişim</HashLink>
+        <Navbar.Toggle 
+          aria-controls="basic-navbar-nav" 
+          className="d-lg-none navbar-toggler" 
+          onClick={() => setIsSidePanelOpen(!isSidePanelOpen)} 
+        />
+        
+        <div className={`side-panel ${isSidePanelOpen ? 'open' : ''}`}>
+          <Nav className="navbar-container">
+            <HashLink className={activeTabIndex === 0 ? "active" : ""} as={Link} to="/#home">Ana Sayfa</HashLink>
+            <HashLink className={activeTabIndex === 1 ? "active" : ""} to="/#about">Hakkımızda</HashLink>
+            <Nav.Link className={activeTabIndex === 2 ? "active" : ""} as={Link} to="/products">Ürünler</Nav.Link>
+            <HashLink className={activeTabIndex === 3 ? "active" : ""} as={Link} to="/#contact">İletişim</HashLink>
+          </Nav>
+        </div>
+
+        <Navbar.Collapse className="d-lg-flex justify-content-between" id="basic-navbar-nav">
+          <Nav className="navbar-container ml-auto">
+            <HashLink className={activeTabIndex === 0 ? "active" : ""} as={Link} to="/#home">Ana Sayfa</HashLink>
+            <HashLink className={activeTabIndex === 1 ? "active" : ""} to="/#about">Hakkımızda</HashLink>
+            <Nav.Link className={activeTabIndex === 2 ? "active" : ""} as={Link} to="/products">Ürünler</Nav.Link>
+            <HashLink className={activeTabIndex === 3 ? "active" : ""} as={Link} to="/#contact">İletişim</HashLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
