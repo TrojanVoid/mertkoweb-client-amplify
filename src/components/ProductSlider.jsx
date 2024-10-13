2683c98ad6a3972c11553c8cfb13baaaad8256ce
TrojanVoid <kiroglu.berk@gmail.com>
2024-10-13 19:28:22 +0300

    product slider change

--- src/components/ProductSlider.jsx
+++ src/components/ProductSlider.jsx
@@ -26,7 +26,7 @@ const ProductSlider = () => {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 5,
+    slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 0,
