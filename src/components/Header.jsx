8aedd32641fb24f2f064071b9e8ee5cec6ed40b8
TrojanVoid <kiroglu.berk@gmail.com>
2024-10-18 16:07:26 +0300

    header fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav, Container, Dropdown } from 'react-bootstrap';
 import { HashLink } from 'react-router-hash-link';
 import { Link } from 'react-router-dom';
 import "../style/components/Header.scss";
