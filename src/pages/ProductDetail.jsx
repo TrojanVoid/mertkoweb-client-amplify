3b0786c28152d301c6cba846501ba68d29809be8
TrojanVoid <kiroglu.berk@gmail.com>
2024-11-14 21:00:40 +0300

    removed unnecessary margin from product detail page slider thumbnail

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -121,7 +121,7 @@ const ProductDetail = () => {
                 ))}
               </Slider>
 
-              <div className="thumbnail-slider mt-3 d-flex justify-content-start align-items-center w-100 h-25">
+              <div className="d-flex justify-content-start align-items-center w-100 h-25">
                 {images.map((image, index) => (
                   <Image
                     key={index}
