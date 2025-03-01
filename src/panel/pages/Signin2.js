5eee3ea0e42678d57de121e4e0ba0500c4ba4ee9
Berk Kıroğlu <kirogluberk@gmail.com>
2025-03-02 00:33:49 +0300

    signin style fix

--- src/panel/pages/Signin2.js
+++ src/panel/pages/Signin2.js
@@ -1,60 +1,36 @@
-import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
 import { Button, Card, Col, Form, Row } from "react-bootstrap";
 import bg1 from "../assets/img/bg1.jpg";
 
 export default function Signin2() {
-  const navigate = useNavigate();
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (username === "mertko" && password === "mertko2025_") {
-      navigate("/dashboard");
-    } else {
-      alert("Kullanıcı adı veya şifre hatalı!");
-    }
-  };
-
   return (
-    <div className="page-sign d-block py-0 panel-wrapper flex justify-center items-center pt-20">
-      <Row className="g-0">
-        <Col md="7" lg="5" xl="4" className="col-wrapper">
-          <Card className="card-sign">
-            <Card.Header className="bg-transparent border-0">
-              <Link to="/" className="header-logo mb-5 text-white"></Link>
-              <Card.Title className="text-white">Giriş Yap</Card.Title>
-              <Card.Text className="text-white">
-                Hoşgeldiniz! Devam etmek için giriş yapınız.
-              </Card.Text>
-            </Card.Header>
+    <div className="page-sign flex py-0 panel-wrapper flex justify-between items-center pt-20 h-[100vh]">
+      <Row className="g-0 h-full w-full">
+        <Col md="7" lg="5" xl="4" className="col-wrapper mt-auto mb-auto">
+          <Card className="card-sign ml-auto mr-auto">
+          <Card.Header className="bg-transparent border-0">
+            <Link to="/" className="header-logo mb-5 text-black-200">MerakliKids</Link>
+            <Card.Title className="text-black-200">Giriş Yap</Card.Title>
+            <Card.Text className="text-black-200">Hoşgeldiniz! Devam etmek için giriş yapınız.</Card.Text>
+          </Card.Header>
             <Card.Body>
-              <Form onSubmit={handleSubmit}>
+              <Form method="get" action="/dashboard/finance">
                 <div className="mb-4">
-                  <Form.Label className="text-white">E-Posta Adresi</Form.Label>
-                  <Form.Control
-                    type="text"
-                    placeholder="Kullanıcı Adı"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                  />
+                  <Form.Label className="text-black-200">E-Posta Adresi</Form.Label>
+                  <Form.Control type="text" placeholder="Enter your email address" value="me@themepixels.com" />
                 </div>
                 <div className="mb-4">
-                  <Form.Label className="d-flex justify-content-between text-white">
-                  </Form.Label>
-                  <Form.Control
-                    type="password"
-                    placeholder="Şifre"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                  />
+                  <Form.Label className="d-flex justify-content-between text-black-200" >
+                    Parola       <Link to="">Şifremi Unuttum?</Link>
+                  </Form.Label> 
+                  <Form.Control type="password" placeholder="Enter your password" value="password123" />     
                 </div>
-                <Button type="submit" className="btn-sign">
-                  Giriş Yap
-                </Button>
+                
+                <Button type="submit" className="btn-sign">Giris Yap</Button>
               </Form>
             </Card.Body>
+          
           </Card>
         </Col>
         <Col className="d-none d-lg-block">
@@ -62,5 +38,5 @@ export default function Signin2() {
         </Col>
       </Row>
     </div>
-  );
+  )
 }
