a5d9eb2e0c563b7831182fcc732fecdefcde3d44
Berk Kıroğlu <kirogluberk@gmail.com>
2025-03-06 04:48:22 +0300

    BU SCSS'LER KUCUK HARF OLCAK DEGISTIRME

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -8,7 +8,7 @@ import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css'
 import HandlePagination from '../util/HandlePagination';
 import Layout from '../global/Layout';
-import '../style/pages/Products.scss';
+import '../style/pages/products.scss';
 
 const {Logger, TITLE_TAGS} = require('../util/Logger');
 const {types, requestByType} = require("../apis/ProductApi");
