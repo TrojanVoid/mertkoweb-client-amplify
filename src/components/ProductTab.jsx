15dd5569dfb0f7a72b1c1d84965e33d276b6df46
Berk Kıroğlu <kirogluberk@gmail.com>
2025-06-15 06:00:48 +0300

    refactor ProductTab to use the InfiniteScroller component as its renderer logic

--- src/components/ProductTab.jsx
+++ src/components/ProductTab.jsx
@@ -1,127 +1,121 @@
-import React, { useEffect, useState } from 'react'
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay, Navigation } from 'swiper/modules';
-import 'swiper/css/bundle';
-import Product from './Product';
+import React, { useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
-const {Logger, TITLE_TAGS} = require("../util/Logger");
-const {types, requestByType} = require("../apis/ProductApi");
+import InfiniteScroller from './InfiniteScroller';
+import Product from './Product';
 
+const { Logger, TITLE_TAGS } = require("../util/Logger");
+const { types, requestByType } = require("../apis/ProductApi");
 const ProductTabTypes = require('../data/ProductTabTypes.json')["productTabTypes"];
-const featuredProductTypes = ProductTabTypes.map((entry) => {
-    return entry["key"];
-});
 
-const ProductTab = ({ data, start, limit }) => {
-    const [activeTab, setActiveTab] = useState('featuredProducts');
-    const [loading, setLoading] = useState(true);
-    const [featuredProducts, setFeaturedProducts] = useState([]);
-    const [bestSellerProducts, setBestSellerProducts] = useState([]);
-    const [newReleaseProducts, setNewReleaseProducts] = useState([]);
-    const [selectedLanguage, setSelectedLanguage] = useState('tr');
-    
-    useEffect(() => {
-        fetchProducts();
-    }, []);
+const featuredProductTypes = ProductTabTypes.map((entry) => entry["key"]);
 
+const ProductTab = ({ start = 0, limit = 12 }) => {
+  const [activeTab, setActiveTab] = useState('featuredProducts');
+  const [loading, setLoading] = useState(true);
+  const [featuredProducts, setFeaturedProducts] = useState([]);
+  const [bestSellerProducts, setBestSellerProducts] = useState([]);
+  const [newReleaseProducts, setNewReleaseProducts] = useState([]);
+  const [selectedLanguage, setSelectedLanguage] = useState('tr');
 
-    const fetchProducts = async () => {
-        try {
-            for(const featureType of featuredProductTypes) {
-              await requestByType(featureType)
-                .then((response) => {
-                  if(!response || !response.data || response.data.length === 0) {
-                      return;
-                  }
-                  if (featureType === 'featuredProducts') {
-                      setFeaturedProducts(response.data);
-                  } else if (featureType === 'bestSellers') {
-                      setBestSellerProducts(response.data);
-                  } else if (featureType === 'newReleases') {
-                      setNewReleaseProducts(response.data);
-                  }
-                  setLoading(false);
-                });
-            }
-        } catch (error) {
-            Logger.error(`Error fetching products: ${error}`, TITLE_TAGS.UI_COMPONENT);
-        }
-    };
+  useEffect(() => {
+    fetchProducts();
+  }, []);
 
-    const handleTabClick = (item) => {
-        setActiveTab(item);
-    }
+  const fetchProducts = async () => {
+    try {
+      for (const featureType of featuredProductTypes) {
+        await requestByType(featureType).then((response) => {
+          if (!response || !response.data || response.data.length === 0) return;
 
-    const getSelectedProductList = () => {
-      switch (activeTab) {
-        case 'bestSellers':
-            return bestSellerProducts;
-        case 'featuredProducts':
-            return featuredProducts;
-        case 'newReleases':
-            return newReleaseProducts;
-        default:
-            return featuredProducts;
+          if (featureType === 'featuredProducts') {
+            setFeaturedProducts(response.data);
+          } else if (featureType === 'bestSellers') {
+            setBestSellerProducts(response.data);
+          } else if (featureType === 'newReleases') {
+            setNewReleaseProducts(response.data);
+          }
+          setLoading(false);
+        });
       }
+    } catch (error) {
+      Logger.error(`Error fetching products: ${error}`, TITLE_TAGS.UI_COMPONENT);
     }
+  };
 
+  const handleTabClick = (item) => {
+    setActiveTab(item);
+  };
 
+  const getSelectedProductList = () => {
+    switch (activeTab) {
+      case 'bestSellers':
+        return bestSellerProducts;
+      case 'featuredProducts':
+        return featuredProducts;
+      case 'newReleases':
+        return newReleaseProducts;
+      default:
+        return featuredProducts;
+    }
+  };
+
+  if (loading) {
     return (
-        <>
-            <div className="tab-features-block md:pt-20 pt-10 w-[90%]">
-                <div className="container">
-                    <div className="heading flex flex-col items-center text-center">
-                        <div className="menu-tab flex flex-shrink justify-center items-center gap-2 p-1 bg-surface rounded-2xl">
-                            {['bestSellers', 'featuredProducts', 'newReleases'].map((item, index) => (
-                                <div
-                                    key={index}
-                                    className={`tab-item relative w-25 md:!w-[33%] text-secondary flex items-center justify-center heading5 py-2 px-5 cursor-pointer duration-500 hover:text-black ${activeTab === item ? 'active' : ''}`}
-                                    onClick={() => handleTabClick(item)}
-                                >
-                                    {activeTab === item && (
-                                        <motion.div layoutId='active-pill' className='absolute !w-33 inset-0 rounded-2xl bg-white'></motion.div>
-                                    )}
-                                    <span className='relative text-center text-sm sm:text-base md:text-lg z-[1]'>
-                                        {ProductTabTypes.find((entry) => entry["key"] === item)[selectedLanguage]["name"]}
-                                    </span>
-                                </div>
-                            ))}
-                        </div>
-                    </div>
+      <div className="flex justify-center items-center min-h-screen">
+        <div className="loader" />
+      </div>
+    );
+  }
 
-                    <div className="list-product hide-product-sold section-swiper-navigation align md:mt-10 mt-6">
-                        <Swiper
-                            spaceBetween={12}
-                            slidesPerView={2}
-                            navigation
-                            loop={true}
-                            modules={[Navigation, Autoplay]}
-                            breakpoints={{
-                                576: {
-                                    slidesPerView: 2,
-                                    spaceBetween: 12,
-                                },
-                                768: {
-                                    slidesPerView: 3,
-                                    spaceBetween: 20,
-                                },
-                                1200: {
-                                    slidesPerView: 3,
-                                    spaceBetween: 30,
-                                },
-                            }}
-                        >
-                            {!loading && getSelectedProductList().slice(start, limit).map((prd, index) => (
-                                <SwiperSlide key={index}>
-                                    <Product data={prd} type='grid' className="w-[30%]"/>
-                                </SwiperSlide>
-                            ))}
-                        </Swiper>
-                    </div>
-                </div>
+  const selectedProducts = getSelectedProductList();
+
+  return (
+    <div className="container py-10 w-[90%] mx-auto">
+      <div className="heading flex flex-col items-center text-center">
+        <div className="menu-tab flex flex-shrink justify-center items-center gap-2 p-2 bg-surface rounded-2xl">
+          {['bestSellers', 'featuredProducts', 'newReleases'].map((item, index) => (
+            <div
+              key={index}
+              className={`tab-item relative min-w-[120px] md:min-w-[150px] text-secondary flex items-center justify-center heading5 py-3 px-6 cursor-pointer duration-500 hover:text-black ${activeTab === item ? 'text-black' : ''}`}
+              onClick={() => handleTabClick(item)}
+            >
+              {activeTab === item && (
+                <motion.div layoutId="active-pill" className="absolute !w-full inset-0 rounded-2xl bg-white" />
+              )}
+              <span className="relative text-center text-sm sm:text-base md:text-lg z-[1]">
+                {ProductTabTypes.find((entry) => entry["key"] === item)[selectedLanguage]["name"]}
+              </span>
             </div>
-        </>
-    )
-}
+          ))}
+        </div>
+      </div>
 
-export default ProductTab
+      <InfiniteScroller
+        items={selectedProducts}
+        itemsPerLoad={limit}
+        deviceItemsPerLoad={{ mobile: 4, tablet: 6, desktop: 8 }}
+        promptType="click"
+        scrollThreshold={0.8}
+        loadMoreButton={
+          <button 
+            type="button" 
+            className="btn bg-cyan-600 text-white text-md tracking-wide font-bold shadow-lg mt-3 px-[5rem] lg:!px-[5rem] mt-3 py-2 lg:!py-3 uppercase 
+            hover:bg-cyan-500 hover:!shadow-sm hover:tracking-tight hover:scale-[95%] 
+            transition-all duration-300 ease-in-out will-change-transform">
+              Daha Fazla
+          </button>
+        }
+      >
+        {(itemsToRender) => (
+          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-[3rem]">
+            {itemsToRender.map((product, index) => (
+              <Product key={product.sku || index} data={product} type="grid" />
+            ))}
+          </div>
+        )}
+      </InfiniteScroller>
+    </div>
+  );
+};
+
+export default ProductTab;
